3a19fdd7695af9d0e7809178f9ff015858750680
Dan McCarthy <daniel.c.mccarthy@gmail.com>
2025-03-25 21:22:51 -0400

    Add a section for attachments.

--- jira-detail.el
+++ jira-detail.el
@@ -151,6 +151,7 @@
           (magit-insert-section-body
             (jira-detail--description issue)
 	    (insert "\n\n")))))
+    (jira-detail--show-attachments key issue)
     (jira-detail--show-comments key)
     (define-key (current-local-map) (kbd "+")
 		(lambda () (interactive) (jira-detail--add-comment key issue)))
@@ -192,6 +193,37 @@
    :callback
    (lambda (data _response)
      (jira-detail--comments key (alist-get 'comments data)))))
+
+(defun jira-detail--show-attachments (key issue)
+  "Display attachments for issue KEY."
+  (let* ((fields (alist-get 'fields issue))
+         (attachments (alist-get 'attachment fields)))
+    (with-current-buffer (get-buffer-create (concat "*Jira Issue Detail: [" key "]*"))
+      (let ((inhibit-read-only t))
+        (goto-char (point-max))
+        (magit-insert-section (jira-issue-attachments nil nil)
+          (magit-insert-heading "Attachments")
+          (magit-insert-section-body
+            (mapcar (lambda (attachment)
+                      (let* ((url (url-generic-parse-url (alist-get 'content attachment)))
+                             ;; FIXME: verify that filename matches
+                             ;; "attachment/content/[0-9]+"
+                             (id (file-name-nondirectory
+                                  (url-filename url)))
+                             (val (list (alist-get 'filename attachment)
+                                        id)))
+                        (magit-insert-section (jira-attachment-section val nil)
+                          (magit-insert-section-body
+                            (insert (format "%-30s %10s %5sB %s\n"
+                                            (alist-get 'filename attachment)
+                                            (alist-get 'mimeType attachment)
+                                            (file-size-human-readable
+                                             (alist-get 'size attachment))
+                                            (jira-fmt-datetime
+                                             (alist-get 'created attachment))))))))
+                      attachments)))
+          (insert "\n")))))
+
 
 (defun jira-detail-show-issue (key)
   "Retrieve and show the detail information of the issue with KEY."
