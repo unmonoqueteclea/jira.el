291a3277fd37c355c2636f603ea1dedfd185052c
Dan McCarthy <daniel.c.mccarthy@gmail.com>
2025-09-05 12:51:49 -0400

    Don't use list item face for the text in the list item.

--- jira-edit.el
+++ jira-edit.el
@@ -91,7 +91,7 @@
   `((,jira-regexp-blockquote
      0 'jira-face-blockquote prepend)
     (,jira-regexp-list-item
-     . font-lock-builtin-face)
+     1 font-lock-builtin-face)
     ;; can't use `jira-regexp-heading' because font-lock can't select
     ;; a face based on the contents of the match.
     (,(rx bol "h1. " (*? not-newline) eol)
