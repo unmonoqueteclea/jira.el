unmonoqueteclea/jira.el

commit b1d3c497065b143bfc9444c553445cad313d5133

Support lists inside blockquotes.

jira-doc.el
@@ -452,7 +452,10 @@ BLOCK is the media node to format."
   "Format content BLOCK to a string with markup."
   (let* ((type (alist-get 'type block))
 	 (attrs (alist-get 'attrs block))
-         (sep (if (string= type "paragraph") "" "\n"))
+         (sep (if (or (string= type "paragraph")
+                      (string= type "blockquote"))
+                  ""
+                "\n"))
          (prefix (if (string= type "listItem")
                      (concat jira-doc--markup-list-prefix " ")
                    ""))
@@ -689,9 +692,11 @@ BODY is the code block content."
 QUOTED-TEXT is the text to quote."
   `(("type" . "blockquote")
     ("content"
-     (("type" . "paragraph")
-      ("content"
-       ,@(jira-doc-build-inline-blocks quoted-text))))))
+     ;; Only paragraphs and lists are allowed inside blockquotes.
+     ;; Marks are also allowed, even though the ADF documentation says
+     ;; they aren't.
+     ,@(let ((blocks (jira-doc-build-lists (list quoted-text))))
+         (mapcan #'jira-doc-build-inline-blocks blocks)))))
 
 (defun jira-doc--build-heading (heading-text)
   "Make an ADF heading node.

jira-edit.el
@@ -87,7 +87,7 @@
 
 (defvar jira-inline-block-keywords
   `((,jira-regexp-mention . 'jira-face-mention)
-    (,jira-regexp-code . 'jira-face-code)
+    (,jira-regexp-code 0 'jira-face-code prepend)
     (,jira-regexp-link . 'jira-face-link)
     (,jira-regexp-task-item 1 font-lock-builtin-face)
     (,jira-regexp-emoji 0 'jira-face-emoji-reference prepend)
@@ -101,8 +101,7 @@
       "bq. "
       (submatch (+ not-newline)
                 (* (or "\r" "\n" "\r\n")
-                   (+ not-newline)))
-      eol))
+                   (+ not-newline)))))
 
 (defconst jira-regexp-heading
   (rx bol "h" (submatch (any "1-6") ". " (*? not-newline)) eol))
@@ -133,7 +132,7 @@
   `((,jira-regexp-blockquote
      0 'jira-face-blockquote prepend)
     (,jira-regexp-list-item
-     1 font-lock-builtin-face)
+     1 font-lock-builtin-face prepend)
     (,jira-regexp-table-row
      . 'jira-face-code)
     (,jira-regexp-toplevel-adf

commit 8456694cbca5f1500fa277eb8285c0bf544d5858

Fix blockquote regression in b1d3c497.

jira-doc.el
@@ -750,8 +750,14 @@ QUOTED-TEXT is the text to quote."
      ;; Only paragraphs and lists are allowed inside blockquotes.
      ;; Marks are also allowed, even though the ADF documentation says
      ;; they aren't.
-     ,@(let ((blocks (jira-doc-build-lists (list quoted-text))))
-         (mapcan #'jira-doc-build-inline-blocks blocks)))))
+     ,@(mapcar #'(lambda (b)
+                   (if (stringp b)
+                       `(("type" . "paragraph")
+                         ("content" .
+                          ,(mapcan #'jira-doc-build-inline-blocks
+                                   (string-split b "\n"))))
+                     b))
+               (jira-doc-build-lists (list quoted-text))))))
 
 (defun jira-doc--build-heading (heading-text)
   "Make an ADF heading node.